5199a4834763f2784d83a46aa52cfc550e408233
Main <mzk114.cube@gmail.com>
2023-09-22 18:59:39 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,12 @@
 </template>
 
 <script>
-import Blog from "./pages/Blog.vue"
-import BlogDetails from "./pages/BlogDetails.vue"
 import AppHead from "./components/AppHead.vue"
 import AppFoot from "./components/AppFoot.vue"
 
 export default {
   name: 'App',
   components: {
-    Blog,
-    BlogDetails,
     AppHead,
     AppFoot,
   }
